63ef2daeeef3dbaef6d991ba536c2eb8043e9ed6
Simeon Pilgrim <simeon.pilgrim@gmail.com>
2019-02-25 22:57:08 +1300

    Add an old school Mandelbrot Set explorer

---
+++ old school graphics/Mandelbrot/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mandlebrot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            m.Resize(pictureBox1.Width, pictureBox1.Height);
+            var data = m.Render();
+            UpdatePicture(data);
+        }
+
+        Mandelbrot m = new Mandelbrot();
+
+
+        void UpdatePicture(int[,] data)
+        {
+            var bm = new Bitmap(m.width, m.height, PixelFormat.Format24bppRgb);
+            for (int j = 0; j < m.height; j++)
+            {
+                for (int i = 0; i < m.width; i++)
+                {
+                    var d = data[j, i];
+
+                    bm.SetPixel(i, j, d == 0 ? Color.Black : Color.FromArgb(ARGBFromYCbCr(d, 255-d, 0)));
+                }
+            }
+
+            this.pictureBox1.Image = bm;
+            this.Refresh();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch(e.KeyChar)
+            {
+            case 'q': m.ZoomOut(); break;
+            case 'e': m.ZoomIn(); break;
+            case 'w': m.MoveUp(); break;
+            case 's': m.MoveDown(); break;
+            case 'a': m.MoveLeft(); break;
+            case 'd': m.MoveRight(); break;
+            case 'r': m.NumUp(); break;
+            case 'f': m.NumDown(); break;
+            case 't': m.ThreshUp(); break;
+            case 'g': m.ThreshDown(); break;
+
+            }
+            var data = m.Render();
+            UpdatePicture(data);
+        }
+
+        static int ARGBFromYCbCr(int Y, int Cb, int Cr)
+        {
+            // constrain inputs to range 0,255
+            Y = Math.Max(0, Math.Min(255, Y));
+            Cb = Math.Max(0, Math.Min(255, Cb));
+            Cr = Math.Max(0, Math.Min(255, Cr));
+
+            // constrain outputs to range 0,255
+            int r = Math.Max(0, Math.Min(255, Y + (int)((1.4f * (Cb - 128)) + 0.5)));
+            int g = Math.Max(0, Math.Min(255, Y + (int)((-0.343f * (Cr - 128) - 0.711f * (Cb - 128)) + 0.5)));
+            int b = Math.Max(0, Math.Min(255, Y + (int)((1.765f * (Cr - 128)) + 0.5)));
+
+            return (int)(0xFF000000 + (r << 16) + (g << 8) + (b));
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            m.Resize(pictureBox1.Width, pictureBox1.Height);
+            var data = m.Render();
+            UpdatePicture(data);
+        }
+    }
+}
---
+++ old school graphics/Mandelbrot/Mandelbrot.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandlebrot
+{
+    public class Mandelbrot
+    {
+        double thresh = 8.0;
+
+        int MandelCount(double cx, double cy, int num)
+        {
+            double x = cx;
+            double y = cy;
+            double xx = x * x;
+            double yy = y * y;
+            int count = 0;
+
+            while (count < num && (xx + yy) < thresh)
+            {
+                y = (2.0 * x * y) + cy;
+                x = xx - yy + cx;
+
+                xx = x * x;
+                yy = y * y;
+                count += 1;
+            }
+
+            return count;
+        }
+
+        public int width = 200;
+        public int height = 200;
+        //l:-0.71783447265625 r:-0.71771240234375 t:-0.25079345703125 b:-0.25067138671875
+        double left = -0.71783447265625;
+        double right = -0.71771240234375;
+        double top = -0.25079345703125;
+        double bottom = -0.25067138671875;
+
+        int max_colours = 255;
+        int max_num = 100;
+
+        int[,] data;
+
+        public int[,] Render()
+        {
+            data = new int[height, width];
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    double cx = left + ((right - left) * i) / (width - 1);
+                    double cy = top + ((bottom - top) * j) / (height - 1);
+                    int count = MandelCount(cx, cy, max_num);
+
+
+                    int cv = count == max_num ? 0 : ((int)(max_colours * (1.0 - ((double)count / (double)max_num)))) + 1;
+
+                    data[j, i] = cv;
+                }
+            }
+            return data;
+        }
+
+        public void Resize(int w, int h) { width = w; height = h; }
+        public void NumUp() { max_num += 20; }
+        public void NumDown() { max_num -= 20; }
+        public void ThreshUp() { thresh *= 2.0; }
+        public void ThreshDown() { thresh *= 0.5; }
+
+        public void MoveUp()
+        {
+            double h = (bottom - top) / 4.0;
+            top -= h;
+            bottom -= h;
+
+            debug();
+        }
+        public void MoveDown()
+        {
+            double h = (bottom - top) / 4.0;
+            top += h;
+            bottom += h;
+
+            debug();
+        }
+        public void MoveLeft()
+        {
+            double w = (right - left) / 4.0;
+            left -= w;
+            right -= w;
+
+            debug();
+        }
+        public void MoveRight()
+        {
+            double w = (right - left) / 4.0;
+            left += w;
+            right += w;
+
+            debug();
+        }
+        public void ZoomIn()
+        {
+            double w = (right - left) / 4.0;
+            left += w;
+            right -= w;
+            double h = (bottom - top) / 4.0;
+            top += h;
+            bottom -= h;
+
+            debug();
+        }
+        public void ZoomOut()
+        {
+            double w = (right - left) / 2.0;
+            left -= w;
+            right += w;
+            double h = (bottom - top) / 2.0;
+            top -= h;
+            bottom += h;
+
+            debug();
+        }
+        void debug()
+        {
+            System.Diagnostics.Debug.WriteLine($"l:{left} r:{right} t:{top} b:{bottom}");
+        }
+    }
+}
